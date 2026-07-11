ac2fa5c5156f86f7df65aa3291c650cd4146655d
Ordinateur <ordinateur@q4os-desktop>
2026-07-11 18:08:33 +0200

    feat: load icons

---
+++ windowsxp/kde2gnome.sh
@@ -0,0 +1,11 @@
+cp ./16x16/actions/undo.png                                       gnome/16x16/actions/undo.png  
+cp ./16x16/actions/redo.png                                       gnome/16x16/actions/redo.png
+cp ./16x16/actions/back.png                                       gnome/16x16/actions/back.png 
+cp ./16x16/actions/up.png                                         gnome/16x16/actions/up.png
+cp ./16x16/actions/foward.png                                     gnome/16x16/actions/foward.png
+cp ./16x16/actions/down.png                                       gnome/16x16/actions/down.png
+
+cp ./16x16/actions/editcopy.png                                   gnome/16x16/actions/editcopy.png   
+cp ./16x16/actions/editpaste.png                                  gnome/16x16/actions/editpaste.png
+cp ./16x16/actions/editcut.png                                    gnome/16x16/actions/editcut.png   
+
